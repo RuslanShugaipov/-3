fc6c08bc7bfa880b9dfc91736937bbd2f23f8689
RuslanShugaipov <58910717+RuslanShugaipov@users.noreply.github.com>
2020-12-06 22:56:35 +0300

    Add files via upload

---
+++ BinaryHeap.cpp
@@ -0,0 +1,215 @@
+#include "BinaryHeap.h"
+#include <iostream>
+
+using namespace std;
+
+////////////
+//BinaryHeap 
+
+//Constructor
+BinaryHeap::BinaryHeap()
+{
+	root = new int[size];
+	HeapSize = 0;
+}
+
+//Destructor
+BinaryHeap::~BinaryHeap()
+{
+	delete[] root;
+}
+
+
+//Searching for an element in the tree by key
+bool BinaryHeap::contains(int key)
+{
+	for (int i = 0; i < HeapSize; i++)
+	{
+		if (key == root[i])
+			return true;
+	}
+	return false;
+}
+
+//Sifting up
+void BinaryHeap::siftUP(int i)
+{
+	int parent = (i - 1) / 2;
+	if (i > 0 && root[parent] < root[i])
+	{
+		int temp = root[i];
+		root[i] = root[parent];
+		root[parent] = temp;
+		siftUP(parent);
+	}
+}
+ 
+//Sifting down
+void BinaryHeap::siftDown(int i)
+{
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
+	int greater;
+	if (right<size && root[right]>root[left])
+		greater = right;
+	else
+		greater = left;
+	if (greater<size && root[greater]>root[i])
+	{
+		int temp = root[i];
+		root[i] = root[greater];
+		root[greater] = temp;
+		siftDown(greater);
+	}
+}
+
+//Heapify
+void BinaryHeap::heapify(int key,int i)
+{
+	//for (int i = (HeapSize - 1) / 2; i >= 0; i--)
+	//	siftDown(i);
+	int parent = (i - 1) / 2;
+	if (key >= root[parent])
+		siftUP(i);
+	else
+		siftDown(i);
+}
+
+//Adding an element to the tree by key
+void BinaryHeap::insert(int key)
+{
+	HeapSize++;
+	root[HeapSize-1] = key;
+	heapify(key,HeapSize-1);
+}
+
+//Deleting a tree element by key
+void BinaryHeap::remove(int key)
+{
+	if (this->size == 0)
+		throw "Heap is empty";
+	int i = 0;
+	while (root[i] != key)
+		i++;
+	root[i] = root[HeapSize - 1];
+	HeapSize--;
+	heapify(key,i);
+}
+
+//Binary heap output as a tree
+void BinaryHeap::out()
+{
+	int i = 0;
+	int k = 1;
+	while (i < HeapSize)
+	{
+		//Simple ouput to show the tree
+		if (i == 0)
+			cout << "   ";
+		else if (i == 1)
+			cout << "  ";
+		//
+		while ((i < k) && (i < HeapSize))
+		{
+			cout << root[i] << ' ';
+			i++;
+		}
+		cout << endl;
+		k = k * 2 + 1;
+	}
+}
+
+//////////////
+//dft_iterator
+Iterator* BinaryHeap::create_dft_iterator()
+{
+
+	return new dft_iterator(root,size,0);
+}
+
+BinaryHeap::dft_iterator::dft_iterator(int* h, int size, int index=0)
+{
+	cur = h;
+	this->index = index;
+	this->size = size;
+}
+
+BinaryHeap::dft_iterator::~dft_iterator()
+{
+	delete cur;
+}
+
+bool BinaryHeap::dft_iterator::has_next()
+{
+	if (index == 0)
+		return true;
+	if (size == 0)
+		return false;
+	if (!stk.isEmpty() && index != 0)
+		return false;
+	else
+		return true;
+}
+
+int BinaryHeap::dft_iterator::next()
+{
+	if (!has_next())
+		throw "The last element in the heap";
+	int var = cur[index];
+	int left = index * 2 + 1, right = index * 2 + 2;
+	if (index == 0)
+		stk.push_back(0);
+	if (right < size)
+	{
+		stk.push_back(right);
+		index = left;
+	}
+	else if (left < size)
+		index = left;
+	else
+	{
+		if (stk.at(stk.get_size() - 1) == 0)
+			stk.pop_back();
+		else
+		{
+			index = stk.at(stk.get_size() - 1);
+			stk.pop_back();
+		}
+		if (size == 1)
+			index = -1;
+	}
+	return var;
+}
+
+//////////////
+//Bft_iterator
+Iterator* BinaryHeap::create_bft_iterator()
+{
+
+	return new bft_iterator(root, size, 0);
+}
+
+BinaryHeap::bft_iterator::bft_iterator(int* h, int size, int index = 0)
+{
+	cur = h;
+	this->index = 0;
+	this->size = size;
+}
+
+BinaryHeap::bft_iterator::~bft_iterator()
+{
+	delete cur;
+}
+
+bool BinaryHeap::bft_iterator::has_next()
+{
+	return index != this->size;
+}
+
+int BinaryHeap::bft_iterator::next()
+{
+	if (!has_next())
+		throw "The last element in the heap";
+	this->index++;
+	return cur[index-1];
+}
---
+++ UnitTestЛР3.cpp
@@ -0,0 +1,75 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\Users\Руслан\source\repos\АлгиСтр(Лр3)\АлгиСтр(Лр3)\BinaryHeap.cpp"
+#include "C:\Users\Руслан\source\repos\АлгиСтр(Лр3)\АлгиСтр(Лр3)\BinaryHeap.h"
+#include "C:\Users\Руслан\source\repos\АлгиСтр(Лр3)\АлгиСтр(Лр3)\LinkedList.h"
+#include "C:\Users\Руслан\source\repos\АлгиСтр(Лр3)\АлгиСтр(Лр3)\LinkedListcpp.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestЛР3
+{
+	TEST_CLASS(BinaryHeapTests)
+	{
+	public:
+
+		TEST_METHOD(contains)
+		{
+			BinaryHeap test;
+			test.insert(1);
+			test.insert(2);
+			test.insert(3);
+			Assert::AreEqual(test.contains(1), true);
+			Assert::AreEqual(test.contains(4), false);
+		}
+
+		TEST_METHOD(insert)
+		{
+			BinaryHeap test;
+			test.insert(1);
+			test.insert(2);
+			test.insert(3);
+			Assert::AreEqual(test.contains(1), true);
+			Assert::AreEqual(test.contains(2), true);
+			Assert::AreEqual(test.contains(3), true);
+		}
+
+		TEST_METHOD(remove)
+		{
+			BinaryHeap test;
+			test.insert(1);
+			test.insert(2);
+			test.insert(3);
+			Assert::AreEqual(test.contains(1), true);
+			Assert::AreEqual(test.contains(2), true);
+			Assert::AreEqual(test.contains(3), true);
+
+			test.remove(3);
+			Assert::AreEqual(test.contains(1), true);
+			Assert::AreEqual(test.contains(2), true);
+			Assert::AreEqual(test.contains(3), false);
+		}
+
+		TEST_METHOD(dft_iterator)
+		{
+			BinaryHeap test;
+			int array[7] = { 6,5,4,3,2,1,0 };
+			int check[7] = { 6,5,3,2,4,1,0 };
+			for (int i = 0; i < 7; i++)
+				test.insert(array[i]);
+			Iterator* heap_dft_iterator = test.create_dft_iterator();
+			for (int i = 0; i < 7; i++)
+				Assert::IsTrue(heap_dft_iterator->next() == check[i]);
+		}
+
+		TEST_METHOD(bft_iterator)
+		{
+			BinaryHeap test;
+			int array[7] = { 6,5,4,3,2,1,0 };
+			for (int i = 0; i < 7; i++)
+				test.insert(array[i]);
+			Iterator* heap_bft_iterator =test.create_bft_iterator();
+			for (int i = 0; i < 7; i++)
+				Assert::IsTrue(heap_bft_iterator->next() == array[i]);
+		}
+	};
+}
